2469a28bfa854df29824a14453afa17928ade702
Bruno Sena <bsena8@hotmail.com>
2024-05-28 13:04:59 -0300

    primeiro commit desafio smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "987654", modelo: "Modelo 1042", imei: "123456899", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "654987", modelo: "Modelo XR", imei: "45613987", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
